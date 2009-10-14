423f8bcdc57168a244e9c2add775dd035b307f30
Markus Kobel <mkobel@lisa.intranet.biko.ch>
2009-10-14 20:00:31 +0200

    started with i18n - login page and preferences pages used for the first tests

--- config/environment.rb
+++ config/environment.rb
@@ -63,6 +63,12 @@ Rails::Initializer.run do |config|
   # to enable "link":onenote://... or "link":blah://... hyperlinks
   config.action_view.sanitized_allowed_protocols = 'onenote'
 
+  # The internationalization framework can be changed 
+  # to have another default locale (standard is :en) or more load paths.
+  # All files from config/locales/*.rb,yml are added automatically.
+  # config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
+  # config.i18n.default_locale = :de
+
   # See Rails::Configuration for more options
 end
 
